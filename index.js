5ff6003079dc9df23bfb22b339cd46032087c5fc
thanhanmuop1 <thanhnd.b23cc152@stu.ptit.edu.vn>
2024-12-25 01:55:51 +0700

    UPDATE

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(cors({
     origin: [
       'http://localhost:3000',
-      'https://lms-be-mts7.onrender.com'
+      'https://lms-be-muon.onrender.com'
     ],
     credentials: true
   }));
