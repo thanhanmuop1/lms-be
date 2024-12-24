d0008182861716835e95f317b3871809fe9a7da3
thanhanmuop1 <thanhnd.b23cc152@stu.ptit.edu.vn>
2024-12-25 01:37:52 +0700

    update

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,18 +1,14 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
-    host: 'bmkxhddui88xlnd3oqst-mysql.services.clever-cloud.com',
-    user: 'ubb1hzetrzofyjuc',
-    password: 'CxrqBtc9aQ1pxtnuKRBv',
-    database: 'bmkxhddui88xlnd3oqst'
+const pool = mysql.createPool({
+  host: 'bmkxhddui88xlnd3oqst-mysql.services.clever-cloud.com',
+  user: 'ubb1hzetrzofyjuc',
+  password: 'CxrqBtc9aQ1pxtnuKRBv',
+  database: 'bmkxhddui88xlnd3oqst',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err.stack);
-        return;
-    }
-    console.log('Connected to the database');
-});
-
-module.exports = db;
+// Export promise wrapper
+module.exports = pool.promise();
