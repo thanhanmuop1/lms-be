012349340e69137626cee9d980c5a1f48dff17fc
thanhanmuop1 <thanhnd.b23cc152@stu.ptit.edu.vn>
2024-12-25 01:46:06 +0700

    upload

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,14 +1,39 @@
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-  host: 'bmkxhddui88xlnd3oqst-mysql.services.clever-cloud.com',
-  user: 'ubb1hzetrzofyjuc',
-  password: 'CxrqBtc9aQ1pxtnuKRBv',
-  database: 'bmkxhddui88xlnd3oqst',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+const db = mysql.createConnection({
+    host: 'bmkxhddui88xlnd3oqst-mysql.services.clever-cloud.com',
+    user: 'ubb1hzetrzofyjuc',
+    password: 'CxrqBtc9aQ1pxtnuKRBv',
+    database: 'bmkxhddui88xlnd3oqst',
+    connectTimeout: 60000,
+    acquireTimeout: 60000,
+    timeout: 60000,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0
 });
 
-// Export promise wrapper
-module.exports = pool.promise();
+function handleDisconnect() {
+    db.connect((err) => {
+        if (err) {
+            console.error('Error connecting to the database:', err.stack);
+            setTimeout(handleDisconnect, 2000);
+            return;
+        }
+        console.log('Connected to the database');
+    });
+
+    db.on('error', function(err) {
+        console.error('Database error:', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST' || 
+            err.code === 'ECONNRESET' ||
+            err.code === 'PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR') {
+            handleDisconnect();
+        } else {
+            throw err;
+        }
+    });
+}
+
+handleDisconnect();
+
+module.exports = db;
