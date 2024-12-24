51ffca04e3c8d554db4b73a22f6b6fc805034905
thanhanmuop1 <thanhnd.b23cc152@stu.ptit.edu.vn>
2024-12-25 03:23:44 +0700

    update

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -63,7 +63,7 @@ const register = async (req, res) => {
         });
 
         // Gửi email xác thực
-        const verificationUrl = `${process.env.BASE_URL}/verify-email/${verificationToken}`;
+        const verificationUrl = `https://frontend-lms-vwe3.onrender.com/verify-email/${verificationToken}`;
         const mailOptions = {
             from: process.env.MAIL_USER,
             to: email,
