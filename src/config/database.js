db5c4a97e91a66dd3fa4a27fae2dfda80212111f
thanhanmuop1 <thanhnd.b23cc152@stu.ptit.edu.vn>
2025-01-09 02:01:01 +0700

    upload

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const db = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'lms',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     connectTimeout: 60000,
     acquireTimeout: 60000,
     timeout: 60000,
